cc19c18d019b0328550449b868b109d0f13a1abf
叶林建 <1512156049@qq.com>
2020-09-09 10:16:17 +0800

    测试text

---
+++ src/views/text.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text">
+        <h1>{{name}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'text',
+        data(){
+            return{
+                name:'小叶'
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
